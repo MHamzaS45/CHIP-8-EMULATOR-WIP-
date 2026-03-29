9ef1e1fc847bb03eb1383f80d9f27652e0effdfd
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2026-03-29 13:55:54 +0300

    Implement Chip8 class with opcode handling

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdint>
+
+class Chip8
+{
+public:
+	uint8_t registers[16]{};
+	uint8_t memory[4096]{};
+	uint16_t index{};
+	uint16_t pc{};
+	uint16_t stack[16]{};
+	uint8_t sp{};
+	uint8_t delayTimer{};
+	uint8_t soundTimer{};
+	uint8_t keypad[16]{};
+	uint32_t video[64 * 32]{};
+	uint16_t opcode;
+};
+
+void Chip8::OP_Ex9E()
+{
+	uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+	uint8_t key = registers[Vx];
+
+	if (keypad[key])
+	{
+		pc += 2;
+	}
+}
